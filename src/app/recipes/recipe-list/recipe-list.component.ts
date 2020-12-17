85509fa16821ecb4540082e5d42a161034d755cd
sawe-daisy <deesawe16@gmail.com>
2020-12-17 18:01:28 +0300

    displaying details on single recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,15 +10,16 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('A Test Recipe', 'yaaaaaaaaaaaaaaaaaaaaaaaay', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  onRecipeSelcted(recipe: any){
-    this.recipeWasSelected.emit()
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe)
+    console.log(this.recipeWasSelected)
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes/recipe';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  recipeWasSelected: Recipe;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.recipeWasSelected)
   }
 
 }
