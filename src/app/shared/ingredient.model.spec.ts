212fa3abc4d20ad11204979a086239e1dedd0d2b
sawe-daisy <deesawe16@gmail.com>
2020-12-17 07:44:03 +0300

    components and classes

---
+++ src/app/shared/ingredient.model.spec.ts
@@ -0,0 +1,7 @@
+import { Ingredient.Model } from './ingredient.model';
+
+describe('Ingredient.Model', () => {
+  it('should create an instance', () => {
+    expect(new Ingredient.Model()).toBeTruthy();
+  });
+});
